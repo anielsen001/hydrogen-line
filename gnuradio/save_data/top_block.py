ddd4e20958938e177282848536d965f3d08aba05
Aaron Nielsen <apn@apnielsen.com>
2024-09-02 16:44:25 -0400

    changed data save directory to /data

--- gnuradio/save_data/top_block.py
+++ gnuradio/save_data/top_block.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-##################################################
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Aug  6 18:31:20 2020
-##################################################
+# GNU Radio version: 3.8.2.0
 
 from distutils.version import StrictVersion
 
@@ -16,22 +18,22 @@ if __name__ == '__main__':
             x11 = ctypes.cdll.LoadLibrary('libX11.so')
             x11.XInitThreads()
         except:
-            print "Warning: failed to XInitThreads()"
+            print("Warning: failed to XInitThreads()")
 
 from PyQt5 import Qt
-from PyQt5 import Qt, QtCore
-from gnuradio import blocks
-from gnuradio import eng_notation
-from gnuradio import gr
 from gnuradio import qtgui
-from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
-from optparse import OptionParser
-import limesdr
 import sip
+from gnuradio import blocks
+from gnuradio import gr
 import sys
-from gnuradio import qtgui
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import limesdr
 
+from gnuradio import qtgui
 
 class top_block(gr.top_block, Qt.QWidget):
 
@@ -57,8 +59,14 @@ class top_block(gr.top_block, Qt.QWidget):
         self.top_layout.addLayout(self.top_grid_layout)
 
         self.settings = Qt.QSettings("GNU Radio", "top_block")
-        self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
 
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
 
         ##################################################
         # Variables
@@ -70,22 +78,18 @@ class top_block(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
         self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
-        	32768, #size
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	tune_freq, #fc
-        	samp_rate, #bw
-        	"", #name
-                1 #number of inputs
+            32768, #size
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            tune_freq, #fc
+            samp_rate, #bw
+            "", #name
+            1 #number of inputs
         )
         self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
         self.qtgui_waterfall_sink_x_0.enable_grid(False)
         self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
 
-        if not True:
-          self.qtgui_waterfall_sink_x_0.disable_legend()
 
-        if "complex" == "float" or "complex" == "msg_float":
-          self.qtgui_waterfall_sink_x_0.set_plot_pos_half(not True)
 
         labels = ['', '', '', '', '',
                   '', '', '', '', '']
@@ -93,7 +97,8 @@ class top_block(gr.top_block, Qt.QWidget):
                   0, 0, 0, 0, 0]
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
                   1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
+
+        for i in range(1):
             if len(labels[i]) == 0:
                 self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
@@ -106,33 +111,43 @@ class top_block(gr.top_block, Qt.QWidget):
         self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
         self.qtgui_sink_x_0 = qtgui.sink_c(
-        	1024, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	tune_freq, #fc
-        	samp_rate, #bw
-        	"", #name
-        	True, #plotfreq
-        	False, #plotwaterfall
-        	False, #plottime
-        	False, #plotconst
+            1024, #fftsize
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            tune_freq, #fc
+            samp_rate, #bw
+            "", #name
+            True, #plotfreq
+            False, #plotwaterfall
+            False, #plottime
+            False #plotconst
         )
         self.qtgui_sink_x_0.set_update_time(1.0/10)
         self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
 
         self.qtgui_sink_x_0.enable_rf_freq(True)
 
+        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.limesdr_source_0 = limesdr.source('1D40F6375A17E0', 0, '', False)
 
 
-        self.limesdr_source_0 = limesdr.source('1D40F6375A17E0', 0, '')
         self.limesdr_source_0.set_sample_rate(samp_rate)
-        self.limesdr_source_0.set_center_freq(tune_freq, 0)
-        self.limesdr_source_0.set_bandwidth(5e6,0)
-        self.limesdr_source_0.set_gain(30,0)
-        self.limesdr_source_0.set_antenna(255,0)
-        self.limesdr_source_0.calibrate(5e6, 0)
 
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/pi/record_chan1.bin', False)
+
+        self.limesdr_source_0.set_center_freq(tune_freq, 0)
+
+        self.limesdr_source_0.set_bandwidth(5e6, 0)
+
+
+
+
+        self.limesdr_source_0.set_gain(30, 0)
+
+
+        self.limesdr_source_0.set_antenna(255, 0)
+
+
+        self.limesdr_source_0.calibrate(5e6, 0)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/data/record_chan1.bin', False)
         self.blocks_file_sink_0.set_unbuffered(False)
 
 
@@ -144,6 +159,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.connect((self.limesdr_source_0, 0), (self.qtgui_sink_x_0, 0))
         self.connect((self.limesdr_source_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
 
+
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
         self.settings.setValue("geometry", self.saveGeometry())
@@ -154,33 +170,51 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_tune_freq(self, tune_freq):
         self.tune_freq = tune_freq
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.tune_freq, self.samp_rate)
-        self.qtgui_sink_x_0.set_frequency_range(self.tune_freq, self.samp_rate)
         self.limesdr_source_0.set_center_freq(self.tune_freq, 0)
+        self.qtgui_sink_x_0.set_frequency_range(self.tune_freq, self.samp_rate)
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.tune_freq, self.samp_rate)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.tune_freq, self.samp_rate)
         self.qtgui_sink_x_0.set_frequency_range(self.tune_freq, self.samp_rate)
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.tune_freq, self.samp_rate)
+
+
+
 
 
 def main(top_block_cls=top_block, options=None):
 
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
+
     tb.start()
+
     tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
 
     def quitting():
         tb.stop()
         tb.wait()
+
     qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
-
 
 if __name__ == '__main__':
     main()
